3a62ddb6cb01d9e353fba430a8573b36475c80ba
Sascha Ittner <sascha.ittner@modusoft.de>
2019-10-18 15:04:27 +0200

    commited missing changes

--- get_source.sh
+++ get_source.sh
@@ -4,8 +4,11 @@ DEB_VERSION=`(cd etherlabmaster && dpkg-parsechangelog | sed -rne 's/^Version: (
 DEB_ORIG="etherlabmaster_$DEB_VERSION.orig.tar.gz"
 
 HG_REPOS="http://hg.code.sf.net/p/etherlabmaster/code"
-HG_COMMIT=`(echo "$DEB_VERSION" | sed -rne 's/^[0-9.]+\+[0-9]*hg([0-9a-z]*).*$$/\1/p')`
+HG_COMMIT=`(echo "$DEB_VERSION" | sed -rne 's/^[0-9.]+\+[0-9]*hg([0-9a-z]*)p([0-9a-z]*).*$$/\1/p')`
 HG_DIR="etherlabmaster-hg"
+
+PATCH_REPOS="http://hg.code.sf.net/u/uecasm/etherlab-patches"
+PATCH_COMMIT=`(echo "$DEB_VERSION" | sed -rne 's/^[0-9.]+\+[0-9]*hg([0-9a-z]*)p([0-9a-z]*).*$$/\2/p')`
 
 function cleanup {
   rm -rf "$HG_DIR"
@@ -15,6 +18,12 @@ set -e
 
 echo "Cloning HG commit $HG_COMMIT..."
 hg clone $HG_REPOS "$HG_DIR" -r $HG_COMMIT
+
+echo "Cloning patchset commit $PATCH_COMMIT..."
+hg clone $PATCH_REPOS "$HG_DIR/.hg/patches" -r $PATCH_COMMIT
+
+echo "Apply patchset..."
+(cd $HG_DIR && hg qpush -a)
 
 echo "Create source archive..."
 (cd $HG_DIR && hg archive "../$DEB_ORIG")
